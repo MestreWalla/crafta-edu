095c355b0f0c58a6854d42cdb9ed6c71a13e1517
vinisebold <vinicius_pereira153@estudante.sesisenai.org.br>
2025-04-24 14:10:31 -0300

    Implementa novas palavras

--- index-port/main.js
+++ index-port/main.js
@@ -46,7 +46,26 @@ function montarPalavra() {
         if (palavraMontada === "BOCA") {
             conteudoResultado = '<img class="icone" src="/assets/icons/boca.png" alt="boca" title="BOCA">';
         } else if (palavraMontada === "BOLA") {
-            conteudoResultado = '<img class="icone" src="/assets/icons//bola.png" alt="bola" title="BOLA">';
+            conteudoResultado = '<img class="icone" src="/assets/icons/bola.png" alt="bola" title="BOLA">';
+        } else if (palavraMontada === "COPO") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/copo.png" alt="copo" title="COPO">';
+        } else if (palavraMontada === "FACA") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/faca.png" alt="faca" title="FACA">';
+        } else if (palavraMontada === "GATO") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/gato.png" alt="gato" title="GATO">';
+        } else if (palavraMontada === "CABO") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/cabo.png" alt="cabo" title="CABO">';
+        } else if (palavraMontada === "BALA") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/bala.png" alt="bala" title="BALA">';
+        } else if (palavraMontada === "MAPA") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/mapa.png" alt="mapa" title="MAPA">';
+        } else if (palavraMontada === "MOLA") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/mola.png" alt="mola" title="MOLA">';
+        } else if (palavraMontada === "SAPO") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/sapo.png" alt="sapo" title="SAPO">';
+        } else if (palavraMontada === "DADO") {
+            conteudoResultado = '<img class="icone" src="/assets/icons/dado.png" alt="dado" title="DADO">';
+        
         } else {
             conteudoResultado = "<strong>" + palavraMontada + "</strong>";
         }
